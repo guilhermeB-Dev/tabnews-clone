fffffd3edeb1914340829a140bbdafbba5f79e08
guilhermeB-Dev <54085225+guilhermeB-Dev@users.noreply.github.com>
2025-03-20 22:52:16 +0000

    added scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Milayne, eu te amo. Se voce me ama, da uma risadinha! 😄</h1>
+  return <h1>Milayne, eu te amo. Se voce me ama, da uma risadinha! 😄</h1>;
 }
 
 export default Home;
