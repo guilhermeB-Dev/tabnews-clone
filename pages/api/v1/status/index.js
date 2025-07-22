8d3391dd11f47256b0a58e8ab5035ec2766889f3
guilhermeB-Dev <54085225+guilhermeB-Dev@users.noreply.github.com>
2025-07-22 23:29:12 +0000

    finished `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows[0]);
-  response.status(200).json("O professor Filipe Deshamps é o melhor");
+  const updatedAt = new Date().toISOString();
+
+  const postgresVersion = await database.query("SHOW server_version;");
+  const postgresVersionResult = postgresVersion.rows[0].server_version;
+
+  const maxConnections = await database.query("SHOW max_connections;");
+  const maxConnectionsResult = maxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersionResult,
+        max_connections: parseInt(maxConnectionsResult),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
